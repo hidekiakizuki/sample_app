05f48fab82d2de26607e0d9d27dff033d6ccd350
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2023-10-17 11:13:33 +0900

    CSP違反のログを詳細に残すようにする

--- app/controllers/csp_violation_report_controller.rb
+++ app/controllers/csp_violation_report_controller.rb
@@ -4,7 +4,15 @@ class CspViolationReportController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def receive
-    logger.debug(request.body.to_json)
+    raw_body = request.body.read
+    json_body = JSON.parse(raw_body)
+    formatted_json = JSON.pretty_generate(json_body)
+    logger.error("CSP Violation Detected: #{formatted_json}")
     head :no_content
+  rescue JSON::ParserError => e
+    logger.error("Failed to Record CSP Violation: Error parsing JSON - #{e.message}. Original content: #{raw_body}")
+    head :bad_request
+  ensure
+    request.body.rewind
   end
 end
--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class MonitoringController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def healthy
     head :no_content
   end
