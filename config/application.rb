9a632c9cae2f7da5ed17cb67b5d9d22eea376299
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-08-31 10:31:41 +0900

    ログのJSON化 (#119)
    * ログのjson化
    
    * Created new branch feature/akizuki/jsonize_logs
    
    * rubocop対応
    パス変更
    
    * rubocop対応

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Src
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 追加
+    config.time_zone = 'Tokyo'
   end
 end
