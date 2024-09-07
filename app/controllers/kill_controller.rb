0878bc8656cc39e6cd7c9ea223fc9082ef91d7dd
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-09-07 16:52:28 +0900

    logのjson化

--- app/controllers/kill_controller.rb
+++ app/controllers/kill_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ErrorsController < ApplicationController
+class KillController < ApplicationController
   def index
     Process.kill('TERM', Process.pid)
   end
