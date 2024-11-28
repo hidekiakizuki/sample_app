b4ccb6fb86006bad6a80d74c7189065891c3efe0
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-11-28 22:25:39 +0900

    rubocop対応

--- app/controllers/async_test_job_controller.rb
+++ app/controllers/async_test_job_controller.rb
@@ -3,7 +3,7 @@
 class AsyncTestJobController < ApplicationController
   def enqueue
     TestJob.perform_later('param1', 2, { param3: '3' })
-    logger.info "■■■■■■ Put Job Test OK"
+    logger.info '■■■■■■ Put Job Test OK'
     head :ok
   end
 end
