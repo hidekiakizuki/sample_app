135320bd6f5fd0ea11512f2699e08e13a8657c3a
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-11-28 23:49:25 +0900

    ログのリアルタイム出力は環境変数でスイッチできるようにする

--- config/initializers/sync_stdout_and_stderr.rb
+++
@@ -1,6 +0,0 @@
-# ECS FargateでFireLensを利用している場合、Firelensが自動で立ち上げるlog routerコンテナにログがリアルタイムに渡されずバッファリングされます。
-# これはそのための対応です。
-# 以下に似た問題がissueに上がっています。
-# https://github.com/aws/aws-sdk-rails/issues/112
-STDOUT.sync = true
-STDERR.sync = true
